4248fcae41d484bd6736531e88502b52d8aad255
rifoag <rifoagenadi@gmail.com>
2017-11-27 21:31:52 +0700

    driver sampahan

--- [done] driver_unit.c
+++ [done] driver_unit.c
@@ -6,6 +6,14 @@ int main(){
 	unit u1 = empty_unit(MakePOINT(1,1));
 	assign_unit(&u1,'W',1);
 	unit u2 = empty_unit(MakePOINT(1,1));
-	assign_unit(&u2,'W',1);
+	assign_unit(&u2,'K',1);
 	printf("%d\n",isequal_unit(u1,u2));
+	swap_unit(&u1, &u2);
+	printf("%c\n", simbol(u1));
+	printf("%c\n", simbol(u2));
+
+	copy_unit(u1, &u2);
+	printf("%c\n", simbol(u2));
+
+	return 0;
 }
