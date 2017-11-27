6d2ab294d19c129b058d50ccc19157b35d72b779
rifoag <rifoagenadi@gmail.com>
2017-11-27 20:26:02 +0700

    driver peta done

--- [done] driver_peta.c
+++ [done] driver_peta.c
@@ -11,20 +11,10 @@ int main(){
 	empty_peta(&M, 10, 15);
 	printf("INI HARUSNYA SEPULUH : %d \n", NBrsEff(M));
 	printf("INI HARUSNYA LIMABELAS : %d \n", NKolEff(M));
-	// Ngetes display peta (sekalian ngetes empty peta)
-	display_peta(M, P1);
-
-	// Ngetes bangun kerajaan
-	// bangun_kerajaan(&M);
-	// display_peta(M, P1);
-
-	// Ngetes taruh king
-	// taruh_king(&M, &P1, &P2);
-	// display_peta(M, P1);
 
 	// Ngetes init peta
 	init_peta(&M, 10, 15, &P1, &P2);
+	display_peta(M, P1);
 
-	
 	return 0;
 }
